5c33f31bf0b12ddeaa7f534f45f517613ee509ec
Lucas Brandão <56313805+lucasbrandao4770@users.noreply.github.com>
2021-10-15 06:27:09 -0300

    Add files via upload

---
+++ C/0) Snippets/input.c
@@ -0,0 +1,11 @@
+void input(char* buffer){
+	char *p;
+
+	fgets(buffer, BUFFER_SIZE, stdin);
+	if((p = strchr(buffer, '\n')) != NULL)
+		*p = '\0';
+}
+
+void clearBuffer(char* buffer){
+	buffer[0] = '\0';
+}
